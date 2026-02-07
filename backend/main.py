77cd1219c330da1fba6bd06c1a3e0443c4f0b534
Utkarsh Singh <utkarshdevendrasingh@gmail.com>
2026-02-07 18:19:04 +0530

    backend added

---
+++ backend/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+@app.post("/review")
+async def review(request: Request):
+    payload = await request.json()
+    print("GITHUB PAYLOAD:", payload)
+
+    return {"msg": "received"}
